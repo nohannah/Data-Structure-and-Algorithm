458035e3086f8b8bc78434862d497f8a20281fe4
nohannah <nohannah508@gmail.com>
2026-02-13 23:49:38 +0600

    delete function

--- Appand.c
+++ Appand.c
@@ -19,8 +19,10 @@ void Display (struct Array arr)
 void Append(struct Array *arr , int x){
     if(arr ->length < arr->size)
     {
-        arr->length++;
+        arr->A [arr->length]=x;
+
     }
+    arr->length++;
 }
 void Insert (struct Array *arr,int index, int x)
 {
@@ -28,17 +30,34 @@ void Insert (struct Array *arr,int index, int x)
     if(index >=0 && index<= arr-> length)
     for(i = arr ->length ; i >index ; i--)
     {
-        arr->A[i]= arr->A[i-1];
-        arr -> A[index]=x;
-        arr-> length++;
+        arr->A[i]= arr->A[i-1];       
     }
+    arr -> A[index]=x;
+    arr-> length++;
+}
+int Delete (struct Array * arr, int index)
+{
+    int x=0;
+    int i ; 
+    if(index >=0 && index <arr->length)
+    {
+        x=arr->A[index];
+        for(i=index ; i < arr-> length-1; i++)
+           arr->A[i]=arr->A[i+1];
+
+           arr->length--;
+           return x;
+    }
+    return 0 ; 
 }
 int main ()
 {
-    struct Array arr={{2,3,4,5,6},10,5};
-    Append(&arr,10);
-    Insert(&arr,1,10);
-    Display(arr);
+    struct Array arr1={{2,3,4,5,6},10,5};
+    Append(&arr1,19);
+    Insert(&arr1,1,10);
+    Delete(&arr1,3);
+    Display(arr1);
+
     return 0;
 }
 
