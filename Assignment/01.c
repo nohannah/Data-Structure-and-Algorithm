e994b142b759cc00124be61613cd3981082b2b9a
nohannah <nohannah508@gmail.com>
2026-04-11 20:06:04 +0600

    ass1

--- Assignment/01.c
+++ Assignment/01.c
@@ -1,3 +1,32 @@
 #include <stdio.h>
-int nums[];
-int n,x* ;
+#include<stdbool.h>
+int *x;
+bool duplicate(int n[],int size){
+    for (int i =0 ; i<size ; i++)
+    {
+        for(int j=i+1; j<size ; j++)
+        {
+            if(n[i]== n[j])
+            {
+               return true;
+            }
+        }
+    }
+    return false;
+
+}
+
+int main(){
+    int nums[]={1,2,3,3};
+    int size =sizeof(nums)/sizeof(nums[0]) ;
+    if(duplicate(nums,size))
+    {
+        printf("true\n");
+    }
+    else 
+    {
+        printf("false/n");
+    }
+
+    return 0;
+}
